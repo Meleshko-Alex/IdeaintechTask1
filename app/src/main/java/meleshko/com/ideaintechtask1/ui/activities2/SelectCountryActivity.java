c09fb6e2e66b0e45bbfe6a3a723d8448655bc26a
Alexandr Meleshko <meleshko_alexandr@mail.ru>
2017-03-27 18:15:27 +0300

    create SelectCountryActiviyt #4_Search

--- app/src/main/java/meleshko/com/ideaintechtask1/ui/activities2/SelectCountryActivity.java
+++ app/src/main/java/meleshko/com/ideaintechtask1/ui/activities2/SelectCountryActivity.java
@@ -1,8 +1,13 @@
 package meleshko.com.ideaintechtask1.ui.activities2;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.l4digital.fastscroll.FastScrollRecyclerView;
@@ -22,6 +27,7 @@ import meleshko.com.ideaintechtask1.ui.adapters.CountryCodeRecyclerAdapter;
 import static java.lang.String.valueOf;
 
 public class SelectCountryActivity extends BaseActivity {
+    private ArrayList<Country> countryList;
 
     @BindView(R.id.recycler_country) FastScrollRecyclerView recycler_country;
 
@@ -32,8 +38,12 @@ public class SelectCountryActivity extends BaseActivity {
         ButterKnife.bind(this);
         setTitleActionBar(getString(R.string.select_country));
 
+        if(countryList == null){
+            countryList = getCountryList();
+        }
+
         recycler_country.setLayoutManager(new LinearLayoutManager(this));
-        CountryCodeRecyclerAdapter adapter = new CountryCodeRecyclerAdapter(getCountryList(), this);
+        CountryCodeRecyclerAdapter adapter = new CountryCodeRecyclerAdapter(countryList, this);
         recycler_country.setAdapter(adapter);
 
     }
@@ -41,7 +51,31 @@ public class SelectCountryActivity extends BaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                ArrayList<Country> tempList = new ArrayList<>();
+
+                for(Country temp : countryList){
+                    if(temp.getName().toLowerCase().contains(newText.toLowerCase())){
+                        tempList.add(temp);
+                    }
+                }
+                CountryCodeRecyclerAdapter adapter = new CountryCodeRecyclerAdapter(tempList, SelectCountryActivity.this);
+                recycler_country.setAdapter(adapter);
+
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
     public ArrayList<Country> getCountryList(){
@@ -84,6 +118,19 @@ public class SelectCountryActivity extends BaseActivity {
             } else{
                 dataList.get(i).setFirstLetter(dataList.get(i).getName().substring(0, 1));
             }
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search
         }
     }
 
