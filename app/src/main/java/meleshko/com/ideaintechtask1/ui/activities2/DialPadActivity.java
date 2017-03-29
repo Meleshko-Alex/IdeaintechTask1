0cd36df64f56a4faa245e5b0af707c66db154737
Alexandr Meleshko <meleshko_alexandr@mail.ru>
2017-03-30 01:13:28 +0300

    create activity_dial_pad.xml

---
+++ app/src/main/java/meleshko/com/ideaintechtask1/ui/activities2/DialPadActivity.java
@@ -0,0 +1,15 @@
+package meleshko.com.ideaintechtask1.ui.activities2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import meleshko.com.ideaintechtask1.R;
+
+public class DialPadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dial_pad);
+    }
+}
